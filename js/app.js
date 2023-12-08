7ce9baf81c416d180715539087f4097fcaa706eb
Ismael <ismagarmon3@gmail.com>
2023-12-08 22:49:55 +0100

    upda

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ import { nav_cookies, nav_no_cookies } from './components.js'
 
 window.onload = () => {
     btn()
+    changeMenu()
 }
 
 const btn = () => {
@@ -13,4 +14,69 @@ const btn = () => {
 
 function destroycookie(nombre) {
     document.cookie = nombre + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+
+
+const changeMenu = () => {
+    let pp1 = document.getElementById('one')
+    let pp2 = document.getElementById('two')
+
+
+    let passp = document.getElementById('passp')
+    let passp2 = document.getElementById('passp2')
+
+    passp.addEventListener('click',() => {
+        pp1.classList.add('none')
+        pp2.classList.remove('none')
+    })
+
+    passp2.addEventListener('click',() => {
+        pp1.classList.add('none')
+        pp2.classList.remove('none')
+    })
+
+    let passp3 = document.getElementById('passp3')
+    let passp4 = document.getElementById('passp4')
+
+    passp3.addEventListener('click',() => {
+        pp1.classList.remove('none')
+        pp2.classList.add('none')
+    })
+
+    passp4.addEventListener('click',() => {
+        pp1.classList.remove('none')
+        pp2.classList.add('none')
+    })
+    
+
+    let pp3 = document.getElementById('three')
+    let pp4 = document.getElementById('four')
+
+
+    let passp5 = document.getElementById('passp5')
+    let passp6 = document.getElementById('passp6')
+
+    passp5.addEventListener('click',() => {
+        pp3.classList.add('none')
+        pp4.classList.remove('none')
+    })
+
+    passp6.addEventListener('click',() => {
+        pp3.classList.add('none')
+        pp4.classList.remove('none')
+    })
+
+    let passp7 = document.getElementById('passp7')
+    let passp8 = document.getElementById('passp8')
+
+    passp7.addEventListener('click',() => {
+        pp3.classList.remove('none')
+        pp4.classList.add('none')
+    })
+
+    passp8.addEventListener('click',() => {
+        pp3.classList.remove('none')
+        pp4.classList.add('none')
+    })
+
 }
--- templates/makeorder.php
+++ templates/makeorder.php
@@ -40,8 +40,298 @@ require '../assets/makeanorder.inc.php';
   </nav>
   <main>
 
+    <div class="firstplate">
+
+      <nav class="m-button">
+        <div class="nav-wrapper">
+          <div class="col s12">
+            <a href="#!" class="breadcrumb">First Plate</a>
+          </div>
+        </div>
+      </nav>
+
+      <div id="one">
+
+        <div class="row flex-cc">
+          <div class="card s4 w-3">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="https://apiimg.iberostar.com/uploads/image/32303/crops/16:9/720/image.jpeg">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">Salmon Salad $15.99<i class="material-icons right">more_vert</i></span>
+              <a class="btn waves-effect waves-light" id="c1">Add</a>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Salmon Salad $15.99<i class="material-icons right">close</i></span>
+              <p>
+                Calories: 350,
+                Protein: 25g,
+                Carbohydrates: 15g,
+                Fat: 20g
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row flex-cc">
+          <div class="card s4 w-3">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="https://media.istockphoto.com/id/1165399909/es/foto/deliciosa-comida-en-un-plato-negro-vista-superior-espacio-de-copia.jpg?s=612x612&w=0&k=20&c=WVPqUu2friqxwlr385Ax-QQ_IvylhR_5wl46P5jQbb4=">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">Grilled Chicken $12.99<i class="material-icons right">more_vert</i></span>
+              <a class="btn waves-effect waves-light" id="c2">Add</a>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Grilled Chicken $12.99<i class="material-icons right">close</i></span>
+              <p>Calories: 300,
+                Protein: 30g,
+                Carbohydrates: 10g,
+                Fat: 15g</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row flex-cc">
+          <div class="card s4 w-3">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="https://www.guiarepsol.com/content/dam/repsol-guia/contenidos-imagenes/comer/nuestros-favoritos/restaurante-casa-pepe-juderia-cordoba/gr-cms-media-featured_images-none-1eee71eb-1540-4641-9c9a-6bd1cf267150-01-guia-repsol-casa-pepe-cordoba-22-de-enero-de-2022-177.jpg.transform/rp-rendition-md/image.jpg">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">Rosse Pasta $14.99<i class="material-icons right">more_vert</i></span>
+              <a class="btn waves-effect waves-light" id="c3">Add</a>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Rosse Pasta $14.99<i class="material-icons right">close</i></span>
+              <p>Calories: 400,
+                Protein: 12g,
+                Carbohydrates: 50g,
+                Fat: 18g</p>
+            </div>
+          </div>
+        </div>
+
+        <ul class="pagination center">
+          <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+          <li class="active"><a href="#!">1</a></li>
+          <li class="waves-effect" id="passp2"><a href="#!">2</a></li>
+          <li class="waves-effect" id="passp"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+        </ul>
+
+      </div>
+
+      <div id="two" class="none">
+
+        <div class="row flex-cc">
+          <div class="card s4 w-3">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="https://www.guiarepsol.com/content/dam/repsol-guia/contenidos-imagenes/comer/nuestros-favoritos/restaurante-casa-pepe-juderia-cordoba/gr-cms-media-featured_images-none-1eee71eb-1540-4641-9c9a-6bd1cf267150-01-guia-repsol-casa-pepe-cordoba-22-de-enero-de-2022-177.jpg.transform/rp-rendition-md/image.jpg">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">Beef Stir-Fry $14.99<i class="material-icons right">more_vert</i></span>
+              <a class="btn waves-effect waves-light" id="c4">Add</a>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Beef Stir-Fry $14.99<i class="material-icons right">close</i></span>
+              <p>Calories: 380,
+                Protein: 22g,
+                Carbohydrates: 25g,
+                Fat: 24g</p>
+            </div>
+          </div>
+        </div>
+
+        <ul class="pagination center">
+          <li class="waves-effect" id="passp3"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+          <li class="waves-effect" id="passp4"><a href="#!">1</a></li>
+          <li class="active"><a href="#!">2</a></li>
+          <li class="disabled"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+        </ul>
+
+      </div>
+
+    </div>
+
+
+
+    <div class="secondplate none">
+
+      <nav class="m-button">
+        <div class="nav-wrapper">
+          <div class="col s12">
+            <a href="#!" class="breadcrumb">First Plate</a>
+            <a href="#!" class="breadcrumb">Second Plate</a>
+          </div>
+        </div>
+      </nav>
+
+      <div id="three">
+
+        <div class="row flex-cc">
+          <div class="card s4 w-3">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="https://www.laparcelacr.com/wp-content/uploads/2014/11/platos-fuertes-menu.jpg">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">Shrimp Scampi $17.99<i class="material-icons right">more_vert</i></span>
+              <a class="btn waves-effect waves-light" id="c5">Add</a>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Shrimp Scampi $17.99<i class="material-icons right">close</i></span>
+              <p>
+                Calories: 350,
+                Protein: 25g,
+                Carbohydrates: 15g,
+                Fat: 20g
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row flex-cc">
+          <div class="card s4 w-3">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="https://www.huariques.com/wp-content/uploads/2010/03/lomo_saltado-1.jpg">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">Pizza $11.99<i class="material-icons right">more_vert</i></span>
+              <a class="btn waves-effect waves-light" id="c6">Add</a>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Pizza $11.99<i class="material-icons right">close</i></span>
+              <p>
+                Calories: 350,
+                Protein: 25g,
+                Carbohydrates: 15g,
+                Fat: 20g
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row flex-cc">
+          <div class="card s4 w-3">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="https://viajes.chavetas.es/wp-content/uploads/albums/hongkong11/comerenmacao02.jpg">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">Teriyaki Salmon $12.99<i class="material-icons right">more_vert</i></span>
+              <a class="btn waves-effect waves-light" id="c7">Add</a>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Teriyaki Salmon $12.99<i class="material-icons right">close</i></span>
+              <p>
+                Calories: 350,
+                Protein: 25g,
+                Carbohydrates: 15g,
+                Fat: 20g
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+        <ul class="pagination center">
+          <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+          <li class="active"><a href="#!">1</a></li>
+          <li class="waves-effect" id="passp5"><a href="#!">2</a></li>
+          <li class="waves-effect" id="passp6"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+        </ul>
+
+      </div>
+
+      <div id="four" class="none">
+
+        <div class="row flex-cc">
+          <div class="card s4 w-3">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="https://www.lazarola.com/wp-content/uploads/2018/04/Screenshot_3.jpg">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">Beef and Broccoli $18.99<i class="material-icons right">more_vert</i></span>
+              <a class="btn waves-effect waves-light" id="c8">Add</a>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Beef and Broccoli $18.99<i class="material-icons right">close</i></span>
+              <p>
+                Calorías: 350,
+                Proteínas: 25g,
+                Carbohidratos: 28g,
+                Grasas: 16g
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <ul class="pagination center">
+          <li class="waves-effect" id="passp7"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+          <li class="waves-effect" id="passp8"><a href="#!">1</a></li>
+          <li class="active"><a href="#!">2</a></li>
+          <li class="disabled"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+        </ul>
+      </div>
+
+    </div>
+
+
+
+    <div class="dessert none">
+
+
+      <nav class="m-button">
+        <div class="nav-wrapper">
+          <div class="col s12">
+            <a href="#!" class="breadcrumb">First Plate</a>
+            <a href="#!" class="breadcrumb">Second Plate</a>
+            <a href="#!" class="breadcrumb">Dessert</a>
+          </div>
+        </div>
+      </nav>
+
+      <div class="row flex-cc">
+        <div class="card s4 w-3">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="activator" src="https://www.circuitogastronomico.com/wp-content/uploads/2022/03/tucson-postre.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">Chocolate Lava Cake $10.99<i class="material-icons right">more_vert</i></span>
+            <a class="btn waves-effect waves-light" id="c9">Add</a>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">Chocolate Lava Cake $10.99<i class="material-icons right">close</i></span>
+            <p>
+              Calorías: 400,
+              Proteínas: 5g,
+              Carbohidratos: 45g,
+              Grasas: 22g
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row flex-cc">
+        <div class="card s4 w-3">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="activator" src="https://saposyprincesas.elmundo.es/wp-content/uploads/2019/07/postres-veraniegos-sin-horno-con-frambuesas.jpg">
+          </div>
+          <div class="card-content">
+            <span class="card-title activator grey-text text-darken-4">Fresh Fruit Salad $4.99<i class="material-icons right">more_vert</i></span>
+            <a class="btn waves-effect waves-light" id="c10">Add</a>
+          </div>
+          <div class="card-reveal">
+            <span class="card-title grey-text text-darken-4">Fresh Fruit Salad $4.99<i class="material-icons right">close</i></span>
+            <p>
+              Calorías: 150, 
+              Proteínas: 2g, 
+              Carbohidratos: 35g, 
+              Grasas: 1g
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
-  
+
 </body>
 
 </html>
